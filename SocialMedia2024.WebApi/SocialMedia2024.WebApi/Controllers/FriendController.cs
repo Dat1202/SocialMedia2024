597843f7e7ca80a6827715a7353c48057a17a7fb
Dat1202 <2051052027dat@ou.edu.vn>
2024-09-06 21:01:37 +0700

    update template email

---
+++ SocialMedia2024.WebApi/SocialMedia2024.WebApi/Controllers/FriendController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia2024.WebApi.Service.Interfaces;
+
+namespace SocialMedia2024.WebApi.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class FriendController : Controller
+    {
+        private readonly IFriendService _friendService;
+
+        public FriendController(IFriendService friendService) 
+        {
+            _friendService = friendService;
+        }
+
+        [HttpPost("add-friend")]
+        public async Task<IActionResult> AddFriend([FromBody] string friendId)
+        {
+            var currentUser = User.FindFirst("UserId")?.Value;
+
+            await _friendService.AddFriend(currentUser, friendId);
+            return Created();
+        }
+
+        [HttpPatch("accept-friend")]
+        public async Task<IActionResult> AcceptFriend([FromBody] string friendId)
+        {
+            var currentUser = User.FindFirst("UserId")?.Value;
+
+            await _friendService.AcceptFriend(currentUser, friendId);
+            return Ok();
+        }
+
+        [HttpPatch("blocked-friend")]
+        public async Task<IActionResult> BlockedFriend([FromBody] string friendId)
+        {
+            var currentUser = User.FindFirst("UserId")?.Value;
+
+            await _friendService.BlockedFriend(currentUser, friendId);
+            return Ok();
+        }
+
+        [HttpPatch("denied-friend")]
+        public async Task<IActionResult> DeniedFriend([FromBody] string friendId)
+        {
+            var currentUser = User.FindFirst("UserId")?.Value;
+
+            await _friendService.DeniedFriend(currentUser, friendId);
+            return Ok();
+        }
+    }
+}
---
+++ SocialMedia2024.WebApi/SocialMedia2024.WebApi/Controllers/TestController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia2024.WebApi.Core.EmailHelper;
+using SocialMedia2024.WebApi.Service.Interfaces;
+
+namespace SocialMedia2024.WebApi.Controllers
+{
+    [AllowAnonymous]
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    public class TestController : HandleController
+    {
+        private readonly IMenuService _ITLMenuService; 
+        private readonly IEmailHelper _emailHelper;
+
+        public TestController(IMenuService ITLMenuService, IErrorCodeService errorService, IMapper mapper, IEmailHelper emailHelper) : base(errorService, mapper) 
+        {
+            _emailHelper = emailHelper;
+            _ITLMenuService = ITLMenuService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTest()
+        {
+            var menuItems = await _ITLMenuService.GetAllDapperSql();
+            if (menuItems == null || menuItems.Count() != 0)
+            {
+                return await ResponseError("Test");
+            }
+
+            await _emailHelper.SendMail(new Domain.SystemEntities.EmailRequest
+            {
+                To = "dat120202@gmail.com",
+                Subject = "Test",
+                Content = "Test"
+            });
+            return await ResponseGet(menuItems);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string name)
+        {
+            await _ITLMenuService.Delete(name);
+            return NoContent(); 
+        }
+
+    }
+}
